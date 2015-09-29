1e2c9ee9c599426aa7c1d0a6dabcc201d33ca85a
w1lat <w1lat@ukr.net>
2015-09-29 09:01:06 +0300

    Update WayBill.java

--- src/main/java/wi/talya/model/WayBill.java
+++ src/main/java/wi/talya/model/WayBill.java
@@ -8,9 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-/**
- * Created by Таня on 02.06.2015.
- */
+
 @Entity
 @Table(name = "way_bills")
 public class WayBill extends GeneratedIdentifierEntity{
